4267bb47eb73304f3622c7a8c4b9022b4383e4c2
coderxguan <3288733739@qq.com>
2025-04-06 20:09:26 +0800

    添加sql初始化脚本

---
+++ backend/src/main/resources/init.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `user` (
+                        `user_id` BIGINT PRIMARY KEY AUTO_INCREMENT COMMENT '用户唯一性id',
+                        `user_name` VARCHAR(50) NOT NULL COMMENT '用户账户名',
+                        `pass_word` VARCHAR(100) NOT NULL COMMENT '用户加密后密码',
+                        `email` VARCHAR(100) COMMENT '用户注册邮箱',
+                        `phone` VARCHAR(20) COMMENT '用户手机号码',
+                        `role` TINYINT DEFAULT 0 COMMENT '用户角色 0-普通用户，1-VIP，2-管理员',
+                        `status` TINYINT DEFAULT 2 COMMENT '用户账户状态 0-正常 1-封禁 2-待激活',
+                        `create_time` DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT '账户注册时间',
+                        `last_login` DATETIME COMMENT '账户最后登录时间',
+                        `api_key` VARCHAR(255) COMMENT '用户midjourney密钥'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='用户类型实体';
