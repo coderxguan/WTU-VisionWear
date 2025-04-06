68d8dbb33a4e4aaca6e78f667ad06048c25aaf64
dghyuiok <14341407+dghyuiok@user.noreply.gitee.com>
2025-04-06 19:45:23 +0800

    封装用户表实体对象

---
+++ backend/src/main/java/com/wtu/entity/User.java
@@ -0,0 +1,49 @@
+package com.wtu.entity;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Schema(description = "用户类型实体")
+public class User implements Serializable {
+    @Schema(description = "用户唯一性id")
+    private Long userId;
+
+    @Schema(description = "用户账户名")
+    private String userName;
+
+    @Schema(description = "用户加密后密码")
+    private String passWord;
+
+    @Schema(description = "用户注册邮箱")
+    private String email;
+
+    @Schema(description = "用户手机号码")
+    private String phone;
+
+    @Schema(description = "用户角色")//0-普通用户，1-VIP，2-管理员
+    private Integer role;
+
+    @Schema(description = "用户账户状态")//0-正常 1-封禁 2-待激活
+    private Integer staus;
+
+    @Schema(description = "账户注册时间")@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createTime;
+
+    @Schema(description = "账户注最后登录时间")@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime lastLogin;
+
+    @Schema(description = "用户midjourney密钥")
+    private String apiKey;
+
+}
