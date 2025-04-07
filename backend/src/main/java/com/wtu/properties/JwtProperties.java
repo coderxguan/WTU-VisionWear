1b6afbc515fbe935234793d10f767b129516b0d2
dghyuiok <14341407+dghyuiok@user.noreply.gitee.com>
2025-04-07 12:55:07 +0800

    封装jwt属性类

---
+++ backend/src/main/java/com/wtu/properties/JwtProperties.java
@@ -0,0 +1,15 @@
+package com.wtu.properties;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "vision.jwt")
+public class JwtProperties {
+    //属性值在application.yml里设置，这个类用来读取和封装
+    private String secretKey;
+    private long ttl;
+    private String tokenName;
+}
